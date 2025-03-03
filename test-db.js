c71bcff4d2030865238b0ce42e766657c4fb33a6
cibaax <sebasfer@outlook.com>
2025-03-03 17:45:41 -0500

    db

--- test-db.js
+++ test-db.js
@@ -2,12 +2,8 @@ import pkg from "pg";
 const { Client } = pkg;
 
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false } // A veces es necesario para Supabase
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Necesario en Supabase
 });
 
 client.connect()
